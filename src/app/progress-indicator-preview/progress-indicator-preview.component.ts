36995160111ba32753ff049fe3ce45bc1ec8c68e
ajocson <jocsonarjie@gmail.com>
2026-06-20 16:19:53 +0800

    Add progress indicator component

--- src/app/progress-indicator-preview/progress-indicator-preview.component.ts
+++ src/app/progress-indicator-preview/progress-indicator-preview.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { TdxButtonSize, TdxButtonVariant } from '../shared/components/button/button.model';
 
 @Component({
   selector: 'app-progress-indicator-preview',
@@ -7,4 +8,56 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./progress-indicator-preview.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProgressIndicatorPreviewComponent {}
+export class ProgressIndicatorPreviewComponent implements OnInit, OnDestroy {
+  readonly totalSteps = 10;
+  readonly TdxButtonSize = TdxButtonSize;
+  readonly TdxButtonVariant = TdxButtonVariant;
+
+  currentStep = 1;
+  percentageProgress = 0;
+
+  private percentageTimer?: ReturnType<typeof setInterval>;
+
+  constructor(private readonly changeDetectorRef: ChangeDetectorRef) {}
+
+  get fractionProgress(): number {
+    return (this.currentStep / this.totalSteps) * 100;
+  }
+
+  get fractionValue(): string {
+    return `${this.currentStep}/${this.totalSteps}`;
+  }
+
+  get percentageValue(): string {
+    return `${this.percentageProgress}%`;
+  }
+
+  get canGoBack(): boolean {
+    return this.currentStep > 1;
+  }
+
+  get canGoNext(): boolean {
+    return this.currentStep < this.totalSteps;
+  }
+
+  ngOnInit(): void {
+    this.percentageTimer = setInterval(() => {
+      this.percentageProgress = (this.percentageProgress + 1) % 101;
+      this.changeDetectorRef.markForCheck();
+    }, 80);
+  }
+
+  ngOnDestroy(): void {
+    if (this.percentageTimer) {
+      clearInterval(this.percentageTimer);
+    }
+  }
+
+  goBack(): void {
+    this.currentStep = Math.max(1, this.currentStep - 1);
+  }
+
+  goNext(): void {
+    this.currentStep = Math.min(this.totalSteps, this.currentStep + 1);
+  }
+}
--- src/app/progress-indicator-preview/progress-indicator-preview.module.ts
+++ src/app/progress-indicator-preview/progress-indicator-preview.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ButtonModule } from '../shared/components/button/button.module';
 import { ProgressIndicatorModule } from '../shared/components/progress-indicator';
 import { ProgressIndicatorPreviewComponent } from './progress-indicator-preview.component';
 
 @NgModule({
   declarations: [ProgressIndicatorPreviewComponent],
   exports: [ProgressIndicatorPreviewComponent],
-  imports: [CommonModule, ProgressIndicatorModule],
+  imports: [ButtonModule, CommonModule, ProgressIndicatorModule],
 })
 export class ProgressIndicatorPreviewModule {}
